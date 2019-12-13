9892b28fc868c85d9ab20d612c0eb3ac5ddbbe6f
Joao Cardoso <jpcardoso97@gmail.com>
2019-12-13 04:25:45 +0000

    add sms and mail when update

--- server/services/cars/update.js
+++ server/services/cars/update.js
@@ -1,14 +1,25 @@
+// _core : mail
+// _core : sms
+
+const mail = new Mail(_req.getString("carro_uid"));
+const sms = new SMS(_req.getString("carro_uid"))
+
 const carro = _db.queryFirst(
   "SELECT id FROM carro WHERE uid = ?::varchar",
   _val.init().add(_req.getString("carro_uid"))
 );
 
-const modelo = _db.get("modelo", _req.getString("modelo_uid"));
+if (_req.getString("modelo_uid") != "undefined") {
+    const modelo = _db.get("modelo", _req.getString("modelo_uid"));
 
-_db.update(
-  "carro",
-  carro.getInt("id"),
-  _val.init().set("modelo_id", modelo.getInt("id"))
-);
+    _db.update(
+      "carro",
+      carro.getInt("id"),
+      _val.init().set("modelo_id", modelo.getInt("id"))
+    );
+    
+    mail.send();
+    sms.send();
+}
 
-_out.json(modelo);
+_out.json(carro);
